48adcf231d13cf1db240129f5437970063fb6670
Laurentiu CM <laurentiu.costachescu22@gmail.com>
2022-07-22 11:26:04 +0300

    Add render cards using mockData

--- Render Cards/app.js
+++ Render Cards/app.js
@@ -1,5 +1,4 @@
 // Drivers Data
-
 const mockData = [
   {
     firstName: "Lewis",
@@ -202,3 +201,76 @@ const mockData = [
     hex: "#37BFDC",
   },
 ];
+// Sorted data by points
+const sortedData = mockData.sort((a, b) => {
+  return b.points - a.points;
+});
+
+const root = document.querySelector("#root");
+
+let place = 1;
+
+// Rendering the data
+const cardData = sortedData.map(
+  ({ firstName, lastName, points, team, number, image, country }) => {
+    const container = document.createElement("div");
+    container.innerHTML = `
+    <div class="container">
+        <div class="card">
+          <div class="card_top">
+          <p class="driver_place">${place++}</p>
+          <div class="points_container">
+            <div>
+              <p class="driver_points">${points}</p>
+              <p class="points">PTS</p>
+            </div>
+            <button onclick="" class="points_btn">Add Point</button>
+          </div>
+        </div>
+        <div class="card-main">
+          <div class="name_country">
+            <div class="driver_name">
+              <span class="name_span"></span>
+              <div class="driver_full-name">
+                <h3 class="driver_first-name">${firstName}</h3>
+                <h2 class="driver_last-name">${lastName}</h2>
+              </div>
+            </div>
+            <div class="driver_flag">
+              <p class="flag">${country}</p>
+            </div>
+          </div>
+          <div class="driver_team">
+            <h3 class="team">${team}</h3>
+          </div>
+          <div class="driver_picture">
+            <div class="bg-pattern">
+              <div class="driver">
+                <p class="driver_number">${number}</p>
+                <img
+                  src="${image}"
+                  alt="pilot"
+                  class="pilot"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    root.appendChild(container);
+  }
+);
+
+// Event Listener on the Buttons
+const addBtn = document.querySelectorAll(".points_btn");
+const driverPoints = document.querySelector(".driver_points");
+let counter = 1;
+
+addBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // initial value + 1
+    // Increment points
+  });
+});
+
